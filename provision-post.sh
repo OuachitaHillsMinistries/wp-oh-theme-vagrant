f911cba7ac06a2b7671f06b0fc72c68e93e3f294
Nathan Arthur <narthur.a@gmail.com>
2016-02-28 13:02:20 -0600

    initial commit

---
+++ provision-post.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+mkdir /var/www/wordpress/wp-content/themes/wp-oh-theme
+git clone https://github.com/OuachitaHillsMinistries/wp-oh-theme /var/www/wordpress/wp-content/themes/wp-oh-theme/
